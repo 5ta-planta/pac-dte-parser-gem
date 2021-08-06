5a99677f0b7daa183a7559be0b98a360aeda5a98
Carlos Orellana <carlos.orellana.igss@gmail.com>
2021-08-06 13:20:16 -0600

    Compilando nueva gema

--- lib/pac/factura_electronica/receptor.rb
+++ lib/pac/factura_electronica/receptor.rb
@@ -47,11 +47,11 @@ class Pac::FacturaElectronica::Receptor
         @distrito  = @xml_hash["gUbiRec"]["dDistr"]
         @provincia  = @xml_hash["gUbiRec"]["dProv"]
 
-        @existe_gIdExt = validar_si_existe_grupoB406 # @xml_hash["gIdExt"].present?
-
+        
         @identificacion_extranjero  = @xml_hash["gIdExt"]["dIdExt"]
         @pais_extranjero  = @xml_hash["gIdExt"]["dPaisExt"]
-
+        
+        @existe_gIdExt = validar_si_existe_grupoB406 # @xml_hash["gIdExt"].present?
 
         @telefono  = @xml_hash["dTfnRec"] 
         @correo  = @xml_hash["dCorElectRec"]
@@ -60,18 +60,13 @@ class Pac::FacturaElectronica::Receptor
     end 
 
     def validar_si_existe_grupoB406
-        puts "Validando si existe el grupo B406"
         if @xml_hash["gIdExt"].present?
-            puts "Si esta presente gIdExt"
             if @identificacion_extranjero.present?  and @pais_extranjero.present?
-                puts "Si esta presente 2"
                 return true
             else
-                puts "Se fue por el else interno"
                 return false
             end
         else
-            puts "Se fue por el else externo"
             return false
         end
     end
