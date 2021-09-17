24f5695dac6040a08c75cee4a7d93ad716660f05
Carlos Alberto Orellana Padilla <corellana@infile.com.gt>
2021-09-17 15:01:16 -0600

    Compilando nueva gema

--- compilar_gema.sh
+++ compilar_gema.sh
@@ -1,5 +1,5 @@
 gem build pac_dte_parser.gemspec
-#gem install ./pac_dte_parser-0.0.1.gem
+gem install ./pac_dte_parser-0.0.1.gem
 git add . 
 git commit -am "Compilando nueva gema"
 git push origin main 
