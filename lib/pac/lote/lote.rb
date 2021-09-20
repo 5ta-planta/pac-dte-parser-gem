85169a880689d7470e811d50dc25bdf07df2daa6
Carlos Alberto Orellana Padilla <corellana@infile.com.gt>
2021-09-20 16:23:58 -0600

    Compilando nueva gema

--- lib/pac/lote/lote.rb
+++ lib/pac/lote/lote.rb
@@ -52,11 +52,11 @@ class Pac::Lote::Lote
         @identificador_para_firma_electronica = @xml_hash["rEnviLoteFe"]["dId"]
         @ambiente_destino = @xml_hash["rEnviLoteFe"]["iAmb"].to_i if @xml_hash["rEnviLoteFe"]["iAmb"].present?
         @facturas = []
-        # @xml_hash["rEnviLoteFe"]["xFe"].each do |factura|
-        #     factura  = Pac::FacturaElectronica::FacturaElectronica.new(factura)
-        #     factura.cargar
-        #     @facturas << factura
-        # end 
+        @xml_hash["rEnviLoteFe"]["xFe"].each do |factura|
+            factura  = Pac::FacturaElectronica::FacturaElectronica.new(factura)
+            factura.cargar
+            @facturas << factura
+        end 
         true
     rescue Exception => e
         p e.backtrace
