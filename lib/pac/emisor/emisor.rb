292dcc5d19139fefd653617eae690a0029f05f96
Carlos Alberto Orellana Padilla <corellana@infile.com.gt>
2021-09-24 11:07:30 -0600

    Compilando nueva version

--- lib/pac/emisor/emisor.rb
+++ lib/pac/emisor/emisor.rb
@@ -1,9 +1,11 @@
 class Pac::Emisor::Emisor
     include Dynamoid::Document
     table name: :emisores, key: :ruc
+    field :tipo_contribuyente
     field :nombre
-    field :tipo
-    field :correo
+    field :digito_verificador
+    field :ruc_afiliado_sfe, :boolean
+    field :resolucion_autorizacion
     field :secreto
     field :estado
     field :sucursales, :array  
