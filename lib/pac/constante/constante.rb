428bbede036d0703c545e7bda6c4dc7f0c0b48e6
Carlos Alberto Orellana Padilla <corellana@infile.com.gt>
2021-10-05 11:05:51 -0600

    Compilando nueva version

---
+++ lib/pac/constante/constante.rb
@@ -0,0 +1,14 @@
+class Pac::Constante::Constante
+
+    def self.tipo_receptor
+        [
+
+            ["Contribuyente","01"],
+            ["Consumidor final","02"],
+            ["Consumidor final","03"],
+            ["Consumidor final","04"]
+        ]
+    end
+
+
+end
