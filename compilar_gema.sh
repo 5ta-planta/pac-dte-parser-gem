0ff8ff8ee6284ba4f1d69ae5e157c536ef8dfa8a
ireyes <ireyes@infile.com.gt>
2021-11-17 16:34:02 -0600

    Compilando nueva version

--- compilar_gema.sh
+++ compilar_gema.sh
@@ -3,6 +3,6 @@ gem build pac_dte_parser.gemspec
 gem install ./pac_dte_parser-0.0.171.gem
 git add . 
 git commit -am "Compilando nueva version "  
-#git pull origin main 
-#git push origin main 
+git pull origin main 
+git push origin main 
 
